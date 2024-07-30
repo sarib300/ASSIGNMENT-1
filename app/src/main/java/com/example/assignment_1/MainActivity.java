944a248728140ec6af42723b7108db124acb6c60
SARIB <muhammadsarib942@gmai.com>
2024-07-30 22:20:35 +0500

    Finished

---
+++ app/src/main/java/com/example/assignment_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.alternativeresources;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.assignment_1.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
